1287265b27e5dd004c6201b35e7a206eb8dd376f
Thomas Sileghem <th.sileghem@gmail.com>
2017-07-24 00:09:43 +0100

    feat: add day,week,month options

--- cli.js
+++ cli.js
@@ -10,7 +10,25 @@ const leftPad = require('left-pad');
 const cli = meow(`
 	Usage
 	  $ 1broker-positions-analyser <positions csv path>
+
+	Options
+	  --day    Display results day by day
+	  --week   Display results week by week
+	  --month  Display results month by month
+
+	Examples
+	  $ 1broker-positions-analyser ~/Downloads/position_history_2017-01-01_2017-07-23.csv --month
+	  2017-06: -0.2677
+	  2017-07: -0.1143
 `);
+
+if (cli.flags.help) {
+	cli.showHelp();
+}
+
+const getKey = getKeyGenerator(
+	['day', 'week', 'month'].find(x => cli.flags[x]) || 'day'
+);
 
 const result = {};
 
@@ -20,7 +38,7 @@ fs.createReadStream(cli.input[0])
 }))
 .on('data', ({'Exit Date': date, 'Profit/Loss': profit}) => {
 	date = new Date(date);
-	const key = `${date.getFullYear()}-${leftPad(date.getMonth() + 1, 2, '0')}-${leftPad(date.getDate(), 2, '0')}`;
+	const key = getKey(date);
 
 	if (!(key in result)) {
 		result[key] = 0;
@@ -35,3 +53,18 @@ fs.createReadStream(cli.input[0])
 		console.log(`${date}: ${result[date] < 0 ? '' : ' '}${result[date].toFixed(4)}`);
 	});
 });
+
+function getKeyGenerator(type) {
+	return ({
+		day: d => `${d.getFullYear()}-${leftPad(d.getMonth() + 1, 2, '0')}-${leftPad(d.getDate(), 2, '0')}`,
+		week: d => `${d.getFullYear()}-${leftPad(getWeek(d), 2, '0')}`,
+		month: d => `${d.getFullYear()}-${leftPad(d.getMonth() + 1, 2, '0')}`
+	})[type];
+}
+
+function getWeek(date) {
+	const firstWeekStart = new Date(date.getFullYear(), 0, 1);
+	firstWeekStart.setDate(firstWeekStart.getDay() - 6);
+
+	return Math.ceil((date.getTime() - firstWeekStart.getTime()) / (7 * 24 * 3600 * 1000));
+}
